f358afa7a26b45d19d5ac5bb61bfc13d76a18ba2
Chip Wolf <hello@chipwolf.uk>
2020-10-21 19:45:04 +0100

    feat!: refactor cluster for starbs.net

--- setup/bootstrap-cluster.sh
+++ setup/bootstrap-cluster.sh
@@ -19,8 +19,10 @@ installFlux() {
   message "installing flux"
   # install flux
   helm repo add fluxcd https://charts.fluxcd.io
-  helm upgrade --install flux --values "$REPO_ROOT"/flux/flux/flux-values.yaml --namespace flux fluxcd/flux
-  helm upgrade --install helm-operator --values "$REPO_ROOT"/flux/helm-operator/flux-helm-operator-values.yaml --namespace flux fluxcd/helm-operator
+  helm repo update
+  kubectl create namespace flux
+  helm upgrade --install flux --values "$REPO_ROOT"/flux/flux/values.yaml --namespace flux fluxcd/flux
+  helm upgrade --install helm-operator --values "$REPO_ROOT"/flux/helm-operator/values.yaml --namespace flux fluxcd/helm-operator
 
   FLUX_READY=1
   while [ $FLUX_READY != 0 ]; do
@@ -38,8 +40,6 @@ installFlux() {
 }
 
 installFlux
-"$REPO_ROOT"/setup/bootstrap-objects.sh
-"$REPO_ROOT"/setup/bootstrap-vault.sh
 
 message "all done!"
 kubectl get nodes -o=wide
